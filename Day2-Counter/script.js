bb1fb3976a8d5c17a56710afc1b0da20f2019646
AYUSH PRAKASH <ayushprakash0511@gmail.com>
2026-04-24 15:17:48 +0530

    Add styles for layout and Counter component
    Added CSS styles for body and Counter component.

--- Day2-Counter/script.js
+++ Day2-Counter/script.js
@@ -1,1 +1,48 @@
+body{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: aquamarine;
+    font-family: Arial, Helvetica, sans-serif;
+}
 
+.Counter{
+    width: 100%;
+    max-width: 400px;
+    height: auto;
+    background: #e47b7b;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+}
+
+
+.Counter h4{
+  font-size:30px;
+}
+
+.Counter h1{
+  font-size: 40px;
+}
+
+.buttons{
+    display: flex;
+    gap: 20px;
+    padding: 20px;
+    justify-content: center;
+   flex-wrap: wrap;
+}
+
+button{
+    width: 6.5rem;
+    height: 3rem;
+    font-size: large;
+    font-weight: bold;
+    border-radius: 15px;
+    border-style: none;
+    background-color: blueviolet;
+    color: white;
+}
