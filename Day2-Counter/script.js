1a103c184da2b9770c9a58f19ebbd49a3bfc9175
AYUSH PRAKASH <ayushprakash0511@gmail.com>
2026-04-24 16:06:30 +0530

    Refactor counter script to implement functionality
    Removed CSS styles and added increment, decrement, and reset functions for counter functionality.

--- Day2-Counter/script.js
+++ Day2-Counter/script.js
@@ -1,48 +1,17 @@
-body{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: aquamarine;
-    font-family: Arial, Helvetica, sans-serif;
+let count=0;
+let display=document.getElementById("count");
+
+
+function increment(){
+    count=count+1;
+    display.innerText=count;
 }
 
-.Counter{
-    width: 100%;
-    max-width: 400px;
-    height: auto;
-    background: #e47b7b;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 15px;
+function reset(){
+    display.innerText="0";
 }
 
-
-.Counter h4{
-  font-size:30px;
-}
-
-.Counter h1{
-  font-size: 40px;
-}
-
-.buttons{
-    display: flex;
-    gap: 20px;
-    padding: 20px;
-    justify-content: center;
-   flex-wrap: wrap;
-}
-
-button{
-    width: 6.5rem;
-    height: 3rem;
-    font-size: large;
-    font-weight: bold;
-    border-radius: 15px;
-    border-style: none;
-    background-color: blueviolet;
-    color: white;
+function decrement(){
+    count=count-1;
+    display.innerText=count;
 }
