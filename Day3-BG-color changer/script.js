142164f50a4ec768a0276823995fdfe456a55b82
AYUSH PRAKASH <ayushprakash0511@gmail.com>
2026-04-25 14:06:51 +0530

    Implement random background color changer
    Add functionality to change background color randomly.

--- Day3-BG-color changer/script.js
+++ Day3-BG-color changer/script.js
@@ -1,1 +1,13 @@
+let display=document.body;
+let btn=document.querySelector("button");
+
+
+function change(){
+    let colors=["red","blue","green","yellow"];
+    let random=Math.floor(Math.random()*colors.length);
+    let randomcolor=colors[random];
+    display.style.background=randomcolor;
+    btn.style.background=randomcolor;
+}
+
 
