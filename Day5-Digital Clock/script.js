bd9ed0d995bba78d5ec1707a5c212b08c8ecdd8c
AYUSH PRAKASH <ayushprakash0511@gmail.com>
2026-04-26 08:16:40 +0530

    Implement digital clock update functionality

--- Day5-Digital Clock/script.js
+++ Day5-Digital Clock/script.js
@@ -1,1 +1,14 @@
+let display = document.getElementById("Clock");
+
+function upDateTime() {
+  let now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  let time=hours+":"+minutes+":"+seconds;
+  display.innerText=time;
+}
+setInterval(upDateTime, 1000);
 
